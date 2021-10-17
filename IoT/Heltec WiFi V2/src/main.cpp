a6092fb3d0f0c8a21a733056b7bce6f3e179f40a
Adrien Carrou <dridriswim@gmail.com>
2021-10-17 02:13:54 -0700

    It works!

--- IoT/Heltec WiFi V2/src/main.cpp
+++ IoT/Heltec WiFi V2/src/main.cpp
@@ -5,112 +5,95 @@
 #include "ArduinoJson.h"
 #include "Adafruit_Sensor.h"
 
-#include <string_view>
-
-
-
 const char *AP_SSID = "kingdom2";
 const char *AP_PWD = "22039622";
 
 String Packet;
-String id, Temp, Humid, Lat, Lon, Smoke;
+float temperature, humidity, longitude, latitude, smoke;
 
-String pack[5];
-  
 WiFiMulti wifiMulti;
 
-void LoraRecieve(){
-    // try to parse packet
+void LoraRecieve()
+{
+  // try to parse packet
   int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-  }
-    // received a packet
-    
 
-    if(LoRa.available()){
-      Serial.println("Packet Received: ");
+  if (LoRa.available())
+  {
+    Serial.println("Packet Received: ");
     Packet = LoRa.readString();
   }
   Serial.println(Packet);
-
-  for(int i=0; i<5; i++){
-      substr(Packet.find("\n"));
-      Packet = substr(Packet.find("\n"));
-      pack[i] = Packet;
-  }
-
+  sscanf(Packet.c_str(),"%f\n%f\n%f\n%f\n", &temperature, &humidity, &longitude, &latitude, &smoke);
   delay(10000);
 }
 
-void postDataToServer() {
- 
+void postDataToServer()
+{
+
   Serial.println("Posting JSON data to server...");
   // Block until we are able to connect to the WiFi access point
-  if (wifiMulti.run() == WL_CONNECTED) {
-     
+  if (wifiMulti.run() == WL_CONNECTED)
+  {
+
     HTTPClient http;
-     
+
     http.begin("http://20.69.111.104:5000/iot");
     http.addHeader("Content-Type", "application/json");
-     
+
     StaticJsonDocument<200> doc;
     // Add values in the document
 
-//char json[] = "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
-
     doc["name"] = "ESP32";
-    doc["temperature"] = pack[0];
-    doc["humidity"] = pack[1];
-    doc["longitude"] = pack[2];
-    doc["latitude"] = pack[3];
-    doc["smoke"] = pack[4];
+    doc["temperature"] = temperature;
+    doc["humidity"] = humidity;
+    doc["longitude"] = longitude;
+    doc["latitude"] = latitude;
+    doc["smoke"] = smoke;
 
-    //doc["time"] = 1351824120;
-   
-    // Add an array.
-    //
-    //JsonArray data = doc.createNestedArray("FireData");
-    //data.add("Info");
-     
+
     String requestBody;
     serializeJson(doc, requestBody);
-     
+
     int httpResponseCode = http.POST(requestBody);
- 
-    if(httpResponseCode>0){
-       
-      String response = http.getString();                       
-       
+
+    if (httpResponseCode > 0)
+    {
+
+      String response = http.getString();
+
       Serial.println(httpResponseCode);
       Serial.println(response);
-     
     }
-    else {
-     
+    else
+    {
+
       //Serial.printf("Error occurred while sending HTTP POST: %s\n", http.errorToString(statusCode).c_str());
-       
     }
-     
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  while (!Serial);
+  while (!Serial)
+    ;
 
   Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Enable*/, false /*Serial Enable*/);
   Serial.println("LoRa Receiver");
 
-  if (!LoRa.begin(915E6, 1)) {
+  if (!LoRa.begin(915E6, 1))
+  {
     Serial.println("Starting LoRa failed!");
-    while (1);
+    while (1)
+      ;
   }
 
   wifiMulti.addAP(AP_SSID, AP_PWD);
-
 }
 
-void loop() {
+void loop()
+{
   LoraRecieve();
   postDataToServer();
 }

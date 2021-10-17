b0c73630c39da933ea7c18e8d8f15b537a1d3eb0
Adrien Carrou <dridriswim@gmail.com>
2021-10-16 22:32:51 -0700

    Added Smoke sensor

--- IoT/Heltec LoRa V2/src/main.cpp
+++ IoT/Heltec LoRa V2/src/main.cpp
@@ -6,23 +6,29 @@
 #include "Adafruit_Sensor.h"
 #include "DHT.h"
 
-
+//Temperature Sensor
 #define DHT11PIN 33
 DHT dht(DHT11PIN, DHT11);
 float currentTemp;
 float currentHumidity;
 
+//GPS
 double Lon, Lat;
 TinyGPSPlus gps;
 static const int RXPin = 17, TXPin = 2;
 static const uint32_t GPSBaud = 9600;
 SoftwareSerial gpsSerial(RXPin, TXPin);
 
+//Smoke Sensor
+#define MQ2PIN (32)
+float sensorValue;
+bool Smoke = false;
 
 
 
 
-void getGPS(){
+
+void wakeGPS(){
     while (gpsSerial.available()){
     gps.encode(gpsSerial.read());
     if (gps.location.isUpdated()){
@@ -36,25 +42,51 @@ void getGPS(){
   }
 }
 
+void getSmoke(){
+  pinMode(MQ2PIN, INPUT);
+  sensorValue = analogRead(MQ2PIN);
+  Serial.print("Sensor Value: ");
+  Serial.println(sensorValue);
+  if(sensorValue > 750)
+    {
+      Smoke = true;
+      Serial.println("Smoke detected!!!!");
+    }else{
+      Smoke = false;
+    }
+}
 
-void getDHT(){
+
+void CompileSensors(){
 
   Serial.println("Temperature: " + (String)currentTemp +  "°C");
   Serial.println("Temperature: " + (String)(1.8*currentTemp+32) + "°F");
   Serial.println("Humidity: " + (String)currentHumidity + "%\n");
   Serial.println("Latitude: " + (String)Lat + "\nLongitude: " + (String)Lon);
+  Serial.println("Smoke Detection: " + (String)Smoke);
 
+  //Sending the Data so it can be parsed by the reciever
+  LoRa.println(1.8*currentTemp+32);
+  LoRa.println(currentHumidity);
+  LoRa.println(Lat);
+  LoRa.println(Lon);
+  LoRa.println(Smoke);
+
+/*
   LoRa.println("Temperature: " + (String)currentTemp +  "°C");
   LoRa.println("Temperature: " + (String)(1.8*currentTemp+32) + "°F");
   LoRa.println("Humidity: " + (String)currentHumidity + "%");
   LoRa.println("Latitude: " + (String)Lat + "\nLongitude: " + (String)Lon);
+  LoRa.println("Smoke Detection: " + (String)Smoke);
+  */
 }
 
 
 void SendLoRaPacket(){
   LoRa.beginPacket();
-  getGPS();
-  getDHT();
+  wakeGPS();
+  getSmoke();
+  CompileSensors();
   LoRa.endPacket();
 }
 
